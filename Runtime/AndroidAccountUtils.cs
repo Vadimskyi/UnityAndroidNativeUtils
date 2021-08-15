f7b8bc26ae6febf0a663308a8102bcd57c97ce7e
vadimskyi <gdvadimskyi@gmail.com>
2021-08-15 22:26:36 +0300

    initial commit

---
+++ Runtime/AndroidAccountUtils.cs
@@ -0,0 +1,15 @@
+﻿/* Copyright (C) 2021 Vadimskyi - All Rights Reserved
+ * Github - https://github.com/Vadimskyi
+ * Website - https://www.vadimskyi.com/
+ * You may use, distribute and modify this code under the
+ * terms of the GPL-3.0 License.
+ */
+
+namespace VadimskyiLab.Android
+{
+
+    public class AndroidAccountUtils
+    {
+    }
+
+}
